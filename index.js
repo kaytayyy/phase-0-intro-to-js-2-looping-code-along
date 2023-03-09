77bee03ebb75cdaa8f665dbac9b722774332b574
Inda Smith <inda.k.smith@gmail.com>
2023-03-09 15:56:17 -0500

    took me some time but I was able to complete the tests.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+let names = [`Guadalupe`, `Ollie`, `Aki`]
+
+function writeCards(names) {
+    let cards = []
+    for (let i = 0; i < names.length; i++) {
+        cards[i] = `Thank you, ${names[i]}, for the wonderful surprise gift!`;
+}
+    return cards
+}
+writeCards(names)
+
+function countDown() {
+    for (let number = 10; number >= 0; number--) {
+        console.log(number)
+    }
+}
+countDown(10) 
